ede552e20b5a593e468a4b01d8132fd4e8b80d7a
jourdain0 <144395568+jourdain0@users.noreply.github.com>
2026-09-16 16:58:12 -0500

    Add: rotting-fruit - submission-2

---
+++ Data Structures & Algorithms/rotting-fruit/submission-2.py
@@ -0,0 +1,34 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        q = deque()
+        time = fresh = 0
+        ROWS, COLS = len(grid), len(grid[0])
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+        # Go through grid, tabulating all fresh fruits and adding
+        # all rotten fruits to the deque
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    fresh += 1
+                if grid[r][c] == 2:
+                    q.append([r, c])
+        
+        # Simulate each minute where the rotten fruits make their adjacent fresh
+        # fruits rotten, stopping when there are no more fresh fruits or we can
+        # no longer make other fruits rotten (queue is empty)
+        while q and fresh > 0:
+            for i in range(len(q)):
+                r, c = q.popleft()
+                for dr, dc in directions:
+                    row, col = r + dr, c + dc
+                    if (row < 0 or col < 0 or
+                        row >= ROWS or col >= COLS or
+                        grid[row][col] != 1):
+                        continue
+                    grid[row][col] = 2
+                    q.append([row, col])
+                    fresh -= 1
+            time += 1
+        
+        return time if fresh == 0 else -1
